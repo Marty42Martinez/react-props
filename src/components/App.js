b2e573622bdb82827406d4b7b7a1987dd5de4578
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-13 16:37:08 -0700

    added Header +styling! Thanks Paige!

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Dog from './Dog';
+import Header from './Header';
 
 export default function App() {
   return (
     <>
+      <Header />
       <Dog name="Marty" age={42} weight="14 stones" />
     </>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Now we are talking, dawg</h1>
+      <img src="./src/assets/m3-logo.png" />
+    </header>
+  );
+}
