4db1d1c5247e3dec49d2dd0157902db45580fe3b
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-13 16:16:02 -0700

    App component, Uses Dog

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dog from './Dog';
+
+export default function App() {
+  return (
+    <>
+      <Dog name="Marty" age={42} weight="14 stones" />
+    </>
+  );
+}
