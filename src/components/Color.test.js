f1d59381ea68b4016744720cf06efab23979d49b
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-14 10:26:20 -0700

    better test

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -4,7 +4,7 @@ import Color from './Color';
 
 describe('Color component', () => {
   it('renders a Color', () => {
-    const wrapper = shallow(<Color />);
+    const wrapper = shallow(<Color name="dark orchid" hex="#9932CC" rgb={{ red: 153, green: 50, blue: 204 }}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
