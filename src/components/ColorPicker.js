64ed5ccbadf1b7d057d2d1481f9e91165a57ccc2
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-14 10:07:15 -0700

    color picker button class component

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ColorPicker extends PureComponent {
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  }
+  clickHandler = event => {
+    console.log(`${this.props.title}`);
+  }
+
+  render() {
+    return (
+      <button onClick={this.clickHandler}>{this.props.title}</button>
+    );
+  }
+}
